91b5de86f650982d8e0375727eb23bf74fc2a47a
Bastien Louerat <b.louerat@gmail.com>
2014-11-10 21:24:03 +0000

    add players

--- main.js
+++ main.js
@@ -27,8 +27,7 @@ $(function() {
     }),
     crossDomain: true
   }).done(function(data) {
-    console.log("done");
-    console.log(data);
+    displayPlayer(data);
     events = data;
   }).fail(function() {
     console.log("Error getting events");
@@ -45,3 +44,50 @@ function addEventToList(event) {
   $("#list-container").prepend(compiledTemplate(event));
 }
 
+function urlParser(url) {
+    var parser = document.createElement('a');
+    parser.href = url;
+    return parser;
+}
+
+var players = {
+  youtube: function(url) {
+    var code = urlParser(url).search.substr(3).split('&')[0];
+    return '<iframe src="http://www.youtube.com/embed/' + code + '?autoplay=1" frameborder="0"><iframe/>';
+  },
+  soundcloud: function(url) {
+    var code = urlParser(url).pathname;
+    return '<iframe src="https://w.soundcloud.com/player/?url=soundcloud.com' + code + '&amp;auto_play=true&amp;buying=false&amp;liking=false&amp;download=false&amp;sharing=false&amp;show_artwork=false&amp;show_comments=false&amp;show_playcount=false&amp;show_user=false&amp;hide_related=true&amp;visual=true&amp;start_track=0&amp;callback=true" class="iframe" scrolling="no" frameborder="no"></iframe>';
+  },
+  vimeo: function(url) {
+    var code = urlParser(url).pathname;
+    return '<iframe src="//player.vimeo.com/video' + code + '?autoplay=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+  }
+}
+
+function play(songs) {
+  var song = _.first(songs);
+  var rest = _.rest(songs);
+  $('.media-box').html(players[song.player](song.url));
+  if(rest.length > 0) {
+    $('.media-button-no').unbind('click').click(function() { play(rest) });
+  } else {
+    $('.media-button-no').attr('disabled', 'disabled');
+  }
+}
+
+function displayPlayer(data) {
+
+  var songs = _.chain(data)
+    .map(function(event) { return _.map(event.media_links, function(link) { return {
+      player: (link.site_name) ? link.site_name.toLowerCase() : '',
+      url:    link.url,
+      event:  event
+    }})})
+    .flatten()
+    .filter(function(event) { return _.has(players, event.player) })
+    .shuffle()
+    .value();
+
+  play(songs);
+}
